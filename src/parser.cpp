38f1c6cf57ed06d92dbc32149b41ac3e4a3f55be
Brad Ben Joya <bradbenjoya6@gmail.com>
2025-12-14 21:06:08 +0200

    Added three commands
    entry: serves as the main entry point to the program
    
    exit: serves as the exsiting point of the program.
    
    print: outputs text to the terminal.

---
+++ src/parser.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "No input file provided\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file) {
+        std::cerr << "Could not open file\n";
+        return 1;
+    }
+
+    std::ofstream transpile("transpile.cpp");
+    if (!transpile.is_open()) {
+        std::cerr << "Failed to create output file\n";
+        return 1;
+    }
+
+    enum class import_libs {
+        none,
+        iostream
+    };
+    import_libs lib = import_libs::none;
+
+    std::string line;
+    std::string output;
+
+    while (std::getline(file, line)) {
+        if (line == "entry;") {
+            output += "int main() {\n\n";
+        } else if (line == "exit;") {
+            output += "}\n\n";
+        } else if (line.substr(0, 6) == "print(") {
+            lib = import_libs::iostream;
+
+            size_t start = line.find('(') + 1;
+            size_t end = line.find(')');
+            std::string content = line.substr(start, end - start);
+
+            output += "std::cout << " + content + " << std::endl;\n";
+        }
+    }
+
+    switch (lib) {
+        case import_libs::none:
+            break;
+        case import_libs::iostream:
+            transpile << "#include <iostream>\n";
+            break;
+    }
+
+    transpile << output;
+    //std::system("clang++ transpile.cpp -o app.exe"); // THIS IS ONLY A PLACEHOLDER IN NO WAY SHAPE OR FORM SHOULD THIS BE IN A RELEASE
+    
+    return 0;
+}
+
